e24f3b912e6620355dd7e201aa917ec50c2f91b8
Dimas Alfiansyah <alfi.dim1612@gmail.com>
2026-09-09 10:53:43 +0700

    feat(routes): add nested layouts and auth routes configuration

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,7 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, prefix, layout, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  index("routes/home.tsx"),
+  layout("routes/_protected/layout/index.tsx", [route("/home", "routes/_protected/home.tsx")]),
+  ...prefix("auth", [index("routes/auth/login.tsx"), route("/logout", "routes/auth/logout.tsx")]),
+] satisfies RouteConfig;

b203d2c886e45cc7dcdd2c5b228a4413865dc3b9
Dimas Alfiansyah <alfi.dim1612@gmail.com>
2026-09-15 10:03:25 +0700

    refactor(root): remove unused font preconnect and stylesheet links

--- app/root.tsx
+++ app/root.tsx
@@ -16,19 +16,6 @@ import { useEffect } from "react";
 import { ErrorSection } from "~/components/error-section";
 import { TooltipProvider } from "~/components/ui/tooltip";
 
-export const links: Route.LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
-];
-
 export const middleware: Route.MiddlewareFunction[] = [
   async ({ request, context }, next) => {
     const state = await createRequestSession(request);
